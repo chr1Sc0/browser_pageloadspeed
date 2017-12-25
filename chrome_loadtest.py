3a35abc76ca1e45362bf18d6bf9658e08c180911
Christian Scotti <cscotti@akamai.com>
2017-12-25 19:52:28 +0100

    Made compliant with Python Linter

--- chrome_loadtest.py
+++ chrome_loadtest.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+"""Use environment Python."""
 #################################################
-_FileName = "chrome_loadtest.py"
 #
 # Description:  Run Chrome performance tests using the Navigation API
 #################################################
-
 import sys
-import os
-import time
-import csv
-import json
+# import time
+# import csv
 import random
-import subprocess
-import select
 import pprint
 from optparse import OptionParser
+import docstring
+
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,8 +20,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException, TimeoutException
 
+_FileName = "chrome_loadtest.py"
 
-PAGE_WAIT_TIMEOUT=15
+PAGE_WAIT_TIMEOUT = 15
 
 
 ToolParams = {
@@ -38,34 +34,36 @@ ToolParams = {
 }
 
 
-class PerfTimings:
+class PerfTimings(object):
 
     def __init__(self):
-
+        """Doc string."""
         self.url = ""
         self.calc_timings = {}
 
-
-    def print_metrics(self):
-
+    def print_output(self):
+        """Print to the stdout the performance timings for each URL."""
         pt_keys = self.calc_timings.keys()
 
-        if len(pt_keys):
+        if pt_keys:
             pt_keys.sort()
 
-
         print
-        print "==========================================================================================================="
-        print "|               URL              |   DNS Res.   |   TCP Conn.  | Backend Time | Frontend Time | Page Load |"
-        print "==========================================================================================================="
-        print "| {url:30.25} | {DNS Resolution:9d} ms | {TCP Connection:9d} ms | {Backend Time:9d} ms | {Frontend Time:10d} ms | {Page Load:6d} ms |".format(**self.calc_timings)
+        print "=============================================================== \
+              ============================================"
+        print "|               URL              |   DNS Res.   |   TCP Conn. \
+               | Backend Time | Frontend Time | Page Load |"
+        print "============================================================== \
+               ============================================="
+        print "| {url:30.25} | {DNS Resolution:9d} ms | {TCP Connection:9d} \
+               ms | {Backend Time:9d} ms | {Frontend Time:10d} ms \
+               | {Page Load:6d} ms |".format(**self.calc_timings)
 
-        print "==========================================================================================================="
+        print "============================================================== \
+               =============================================="
 
-
-
-    def collect_perf_timings(self, url):
-
+    def collect_performance_timings(self, url):
+        """Run the chromedriver to collect the performance timers for the passed url."""
         self.url = url
         self.calc_timings.clear()
 
@@ -82,7 +80,6 @@ class PerfTimings:
         # set page load time out to 60 seconds
         driver.set_page_load_timeout(60)
 
-
         # Short routine to verify if the document is fully loaded
         def document_ready(driver):
             test = "return document.readyState"
@@ -90,25 +87,24 @@ class PerfTimings:
                 result = driver.execute_script(test)
                 if result == "complete":
                     return True
-                else:
-                    return False
-            except WebDriverException, e:
                 return False
-
+            except WebDriverException:
+                return False
 
         try:
             # Get the URL and wait until the document is in complete state
-            # This is particularly useful in case of redirections as the get would
-            # return without having the final page fully loaded
+            # This is particularly useful in case of redirections as the get
+            # would return without having the final page fully loaded
             driver.get(current_url)
-            wait = WebDriverWait(driver, PAGE_WAIT_TIMEOUT).until(document_ready)
+            WebDriverWait(driver, PAGE_WAIT_TIMEOUT).until(document_ready)
 
-        except TimeoutException, e:
+        except TimeoutException:
             # If there's a timeout still follow to collect the metrics
             pass
 
-         # Pull the performance timing data
-        tmp_perf_timings = driver.execute_script("return window.performance.timing")
+        # Pull the performance timing data
+        tmp_perf_timings = driver.execute_script(
+            "return window.performance.timing")
         tmp_perf_timings['url'] = str(current_url)
 
         # Caculate the timers
@@ -118,72 +114,77 @@ class PerfTimings:
         driver.quit()
         return True
 
-
     def calc_timers(self, tmp_perf_timings):
 
-        if len(tmp_perf_timings) == 0:
+        if not tmp_perf_timings:
             return
 
-        tmp_cal_timings = dict()
+        tmp_timings = dict()
 
-        tmp_cal_timings["url"] = tmp_perf_timings["url"].strip()
-        tmp_cal_timings["DNS Resolution"] = tmp_perf_timings["domainLookupEnd"] - \
+        tmp_timings["url"] = tmp_perf_timings["url"].strip()
+        tmp_timings["DNS Resolution"] = tmp_perf_timings["domainLookupEnd"] - \
             tmp_perf_timings["domainLookupStart"]
-        tmp_cal_timings["TCP Connection"] = tmp_perf_timings["connectEnd"] - \
+        tmp_timings["TCP Connection"] = tmp_perf_timings["connectEnd"] - \
             tmp_perf_timings["connectStart"]
         if tmp_perf_timings["secureConnectionStart"] > 0:
-            tmp_cal_timings["SSL Handshake"] = tmp_perf_timings["connectEnd"] - \
-            tmp_perf_timings["secureConnectionStart"]
-        tmp_cal_timings["DOM Loading"] = tmp_perf_timings["domLoading"] - \
+            tmp_timings["SSL Handshake"] = tmp_perf_timings["connectEnd"] - \
+                tmp_perf_timings["secureConnectionStart"]
+        tmp_timings["DOM Loading"] = tmp_perf_timings["domLoading"] - \
             tmp_perf_timings["navigationStart"]
-        tmp_cal_timings["DOM Ready"] = tmp_perf_timings["domComplete"] - \
+        tmp_timings["DOM Ready"] = tmp_perf_timings["domComplete"] - \
             tmp_perf_timings["navigationStart"]
-        tmp_cal_timings["Page Load"] = tmp_perf_timings["loadEventEnd"] - \
+        tmp_timings["Page Load"] = tmp_perf_timings["loadEventEnd"] - \
             tmp_perf_timings["navigationStart"]
-        tmp_cal_timings["Backend Time"] = tmp_perf_timings["responseStart"] - \
+        tmp_timings["Backend Time"] = tmp_perf_timings["responseStart"] - \
             tmp_perf_timings["navigationStart"]
-        tmp_cal_timings["Frontend Time"] = tmp_perf_timings["loadEventEnd"] - \
+        tmp_timings["Frontend Time"] = tmp_perf_timings["loadEventEnd"] - \
             tmp_perf_timings["responseStart"]
 
-        #pp = pprint.PrettyPrinter(indent=4)
-        #pp.pprint(tmp_cal_timings)
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(tmp_timings)
 
-        self.calc_timings = tmp_cal_timings
-
+        self.calc_timings = tmp_timings
 
     def run(self, url):
 
-        if self.collect_perf_timings(url):
-            #print self.calc_timings
-            self.print_metrics()
+        if self.collect_performance_timings(url):
+            # print self.calc_timings
+            self.print_output()
 
 
 if __name__ == "__main__":
 
     parser = OptionParser(
-        usage="Usage: {0} --file=<urls> [--iterations=<iterations>]".format(_FileName))
-    parser.add_option("-f", "--file", dest="url_file",
+        usage="Usage: {0} --file=<urls> \
+               [--iterations=<iterations>]".format(_FileName))
+    parser.add_option("-f", "--file",
+                      dest="url_file",
                       help="input file with URLs list")
-    parser.add_option("-n", "--iterations", dest="iterations",
-                      help="Number of iterations", default='1')
-    parser.add_option("-v", "--verbose", dest="verbose", action = 'store_true',
-                      help="include additional SSL and DOM timers", default=False)
+    parser.add_option("-n",
+                      "--iterations",
+                      dest="iterations",
+                      help="Number of iterations",
+                      default='1')
+    parser.add_option("-v", "--verbose",
+                      dest="verbose",
+                      action='store_true',
+                      help="include additional SSL and DOM timers",
+                      default=False)
 
     (options, args) = parser.parse_args()
 
     if not options.url_file:   # if filename is not given
-      parser.error('Filename not given')
+        parser.error('Filename not given')
 
     try:
         with open(options.url_file, 'r') as url_file:
-           test_urls = url_file.readlines()
+            test_urls = url_file.readlines()
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
         exit(2)
-    except:
+    except Exception:
         print "Unexpected error:", sys.exc_info()[0]
         exit(3)
-
 
     pt = PerfTimings()
 
@@ -191,8 +192,8 @@ if __name__ == "__main__":
     random.shuffle(test_urls)
 
     if options.iterations == '1':
-       for i, url in enumerate(test_urls):
-           pt.run(url)
+        for i, url in enumerate(test_urls):
+            pt.run(url)
     else:
         for x in range(0, int(options.iterations)):
             for i, url in enumerate(test_urls):
